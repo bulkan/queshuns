ccb7bddb5624512181278837b0b4ecf14098e93d
unknown <bevcimen@.melb.str.com.au>
2009-12-09 17:00:48 +1100

    added support to push tweets to Redis

--- filter_daemon.py
+++ filter_daemon.py
@@ -1,32 +1,74 @@
 from __future__ import with_statement
 
+from datetime import datetime
+
+import time
+
 import redis
 import tweetstream
 
 
 
+try:
+    import simplejson as json
+except:
+    import json
+
+
 class FilterRedis(object):
 
+    key = "tweets"
+    r = redis.Redis(host='10.1.0.66')
+    r.connect()
+    num_tweets = 20
+    trim_threshold = 100
+
     def __init__(self):
-
-        self.key = "tweets"
-        self.redis = redis.Redis(host='10')
-
-
+        self.trim_count = 0
 
 
     def push(self, data):
-        self.redis.pus
-        
+        self.r.push(self.key, data)
+
+        self.trim_count += 1
+        if self.trim_count >= self.trim_threshold:
+            self.r.ltrim(self.key, 0, self.num_tweets)
+            self.trim_count = 0
+
+    def tweets(self, limit=15, since=0):
+        #for data in self.r.lrange(self.key, 0, limit -1):
+        #    tweet = json.loads(data)
+
+        return [json.loads(x) for x in self.r.lrange(self.key, 0, limit - 1) if json.load(s)['received_at'] <= since]
 
 
 
+    #@db.list_range(REDIS_KEY, 0, limit - 1).collect {|t|
+    #  Tweet.new(JSON.parse(t))
+    #}.reject {|t| t.received_at <= since}  # In 1.8.7, should use drop_while instead
 
-words = ["why", "how", "when"]
 
-with tweetstream.TrackStream("placidified", "ishopsin3021", words) as stream:
-    for tweet in stream:
-        if '@' in tweet['text'] or not tweet['text'].endswith('?'):
-            continue
-        print tweet['user']['screen_name'],':', tweet['text'].encode('utf-8')
-    
+if __name__ == '__main__':
+
+
+    fr = FilterRedis()
+
+    words = ["why", "how", "when", "lol", "feeling"]
+
+
+
+    with tweetstream.TrackStream("placidified", "ishopsin3021", words) as stream:
+        for tweet in stream:
+            if 'text' not in tweet: continue
+            if '@' in tweet['text'] or not tweet['text'].endswith('?'):
+                continue
+            fr.push(json.dumps( {'id':tweet['id'],
+                                 'text':tweet['text'],
+                                 'username':tweet['user']['screen_name'],
+                                 'userid':tweet['user']['id'],
+                                 'name':tweet['user']['name'],
+                                 'profile_image_url':tweet['user']['profile_image_url'],
+                                 'received_at':time.time()}
+                                 )
+                    )
+            print tweet['user']['screen_name'],':', tweet['text'].encode('utf-8')
