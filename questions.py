139b4950200693ece01de053a0bcf5496a42446d
Bulkan Evcimen <bulkan@hardy2.(none)>
2009-12-15 10:32:30 +0000

    New tweets update in IE7 now

--- questions.py
+++ questions.py
@@ -4,6 +4,11 @@ import cherrypy
 import jinja2
 from filter_daemon import *
 
+try:
+    import json
+except:
+    import simplejson as json
+
 from simplejson import JSONEncoder
 encoder = JSONEncoder()
 
@@ -39,13 +44,31 @@ class Questions(object):
         return render_template('index', tweets=tweets)
 
     @cherrypy.expose()
-    def latest(self, since):
+    @cherrypy.tools.jsonify()
+    def latest(self, since, nt):
+        #cherrypy.response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate';
+        #cherrypy.response.headers['Cache-Control'] = 'post-check=0, pre-check=0';
+        #cherrypy.response.headers['Pragma'] = 'no-cache';
+
+        cherrypy.response.headers['Expires'] = 'Sun, 19 Nov 1978 05:00:00 GMT'
+        cherrypy.response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0'
+        cherrypy.response.headers['Pragma'] = 'no-cache'
+        #cherrypy.response.headers['Content-Type'] = "application/json"
+
+
         if not since:
             since = 0
         tweets = self.fr.tweets(limit=5, since=float(since))
 
         return render_template('tweets', tweets=tweets)
 
-#cherrypy.config.update({'server.socket_host': '0.0.0.0',
-#                           'server.socket_port': 8085})
+cherrypy.config.update({'server.socket_host': '0.0.0.0',
+                           'server.socket_port': 8085})
+
+#engine = cherrypy.engine
+#from cherrypy.process import plugins, servers
+#cherrypy.config.update({'log.screen': False})
+#plugins.Daemonizer(engine).subscribe()
+
+#engine.start()
 cherrypy.quickstart(Questions())
