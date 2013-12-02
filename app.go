f19425bf9fcdee913eea799d1a5a6117def329ca
Bulkan Evcimen <bulkan@gmail.com>
2013-12-02 23:22:58 +1100

    getting tweets out of Redis
    need to use a type FilterRedis struct and attach methods on to it
    like Init, Tweets, Latest
    Tweets returned from redis are in []string where each element is JSON

--- app.go
+++ app.go
@@ -3,14 +3,53 @@ package main
 import (
     "fmt"
     "net/http"
+    //"encoding/json"
+    "github.com/gorilla/mux"
+    "github.com/garyburd/redigo/redis"
 )
 
+// global to store redis connection
+var c redis.Conn
 
-func latest(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "/latest handler")
+func LatestHandler(w http.ResponseWriter, r *http.Request) {
+
+    tweets, err := redis.Strings(c.Do("LRANGE", "tweets", 0, 15))
+    if (err != nil) {
+        fmt.Println("Some error occured")
+    }
+
+    // Parse/Unmarshall tweets
+    fmt.Printf("%T\n", tweets)
+    w.Header().Add("Content-Type", "application/json")
+
+    for _, tweet := range tweets {
+        fmt.Println(tweet)
+        fmt.Fprintf(w, tweet)
+    }
+
+    //b, err := json.Marshal(tweets)
+
+    //if (err != nil){
+        //fmt.Println("here")
+    //}
+
+    //defer c.Close()
 }
 
 func main() {
-    http.HandleFunc("/latest", latest)
+
+    var err error
+
+    c, err = redis.Dial("tcp", ":6379")
+    if err != nil {
+        panic(err)
+    }
+
+    r := mux.NewRouter()
+    r.HandleFunc("/latest", LatestHandler).Methods("GET")
+
+    http.Handle("/", r)
+    fmt.Println("listening")
     http.ListenAndServe(":8080", nil)
+
 }
