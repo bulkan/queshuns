4ff35a0f4afda9a78186e1a7d459f801144550f3
Bulkan Evcimen <bulkan@gmail.com>
2014-07-14 22:54:17 +1000

    each tweet stored in redis is a json string
    * thus Unmarshall them into an []Tweet which is then json.Marshal'led

--- app.go
+++ app.go
@@ -3,35 +3,51 @@ package main
 import (
     "fmt"
     "net/http"
-    //"encoding/json"
+    "encoding/json"
     "github.com/gorilla/mux"
     "github.com/garyburd/redigo/redis"
 )
+
+type Tweet struct {
+  Username string `json:"username"`
+  Name string `json:"name"`
+  Profile_image_url string `json:"profile_image_url"`
+  Text string `json:"text"`
+  Userid uint64 `json:"userid"`
+  Id uint64 `json:"id"`
+  Received_at float32 `json:"received_at"`
+}
 
 // global to store redis connection
 var c redis.Conn
 
 func LatestHandler(w http.ResponseWriter, r *http.Request) {
 
-    tweets, err := redis.Strings(c.Do("LRANGE", "tweets", 0, 15))
+    tweet_strings, err := redis.Strings(c.Do("LRANGE", "tweets", 0, 15))
     if (err != nil) {
         fmt.Println("Some error occured")
     }
 
-    // Parse/Unmarshall tweets
-    fmt.Printf("%T\n", tweets)
+    // Unmarshall tweets
+    fmt.Printf("%T\n", tweet_strings)
     w.Header().Add("Content-Type", "application/json")
 
-    for _, tweet := range tweets {
+    var tweets []Tweet
+
+    for _, twit := range tweet_strings {
+        var tweet Tweet
+
+        if err := json.Unmarshal([]byte(twit), &tweet); err != nil {
+    		    fmt.Println("Error parsing JSON: ", err)
+    	  }
+
         fmt.Println(tweet)
-        fmt.Fprintf(w, tweet)
+        tweets = append(tweets, tweet)
+
     }
 
-    //b, err := json.Marshal(tweets)
-
-    //if (err != nil){
-        //fmt.Println("here")
-    //}
+    json_tweets, err := json.Marshal(tweets)
+    fmt.Fprintf(w, string(json_tweets))
 
     //defer c.Close()
 }
